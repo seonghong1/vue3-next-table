4af1f6d65f931e289ca56a4152dbc0f0896ee51e
seonghongjo <jsh@allminwon.com>
2025-01-11 18:26:19 +0900

    프로젝트 init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TableView from "./components/TableView.vue";
+</script>
+
+<template>
+  <h3>테이블 제목입니다</h3>
+  <TableView />
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/components/TableView.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const items = ref([
+  { id: 1, name: "John Doe", age: 30 },
+  { id: 2, name: "Jane Doe", age: 25 },
+  { id: 3, name: "Jim Doe", age: 40 },
+]);
+</script>
+
+<template>
+  <div class="table-view-container">
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Age</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.age }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped lang="scss">
+h1 {
+  color: red;
+
+  span {
+    color: blue;
+  }
+}
+</style>
